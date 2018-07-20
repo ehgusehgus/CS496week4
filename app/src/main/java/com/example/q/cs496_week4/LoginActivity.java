a4a8a9a0b771c34f0868bd2833963f28c1ac983b
sambong0113 <sambong0113@gmail.com>
2018-07-20 21:26:11 +0900

    implement login activity

---
+++ app/src/main/java/com/example/q/cs496_week4/LoginActivity.java
@@ -0,0 +1,54 @@
+package com.example.q.cs496_week4;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+
+public class LoginActivity extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        Intent intent = new Intent(getApplication(),MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancel() {
+                        Toast.makeText(getApplicationContext(), "LoginCancel", Toast.LENGTH_LONG).show();
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        Toast.makeText(getApplicationContext(), "LoginError", Toast.LENGTH_LONG).show();
+                        Log.d("LOGIN", exception.toString());
+                        // App code
+                    }
+                });
+
+    }
+
+
+}
--- app/src/main/java/com/example/q/cs496_week4/MainActivity.java
+++ app/src/main/java/com/example/q/cs496_week4/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.q.cs496_week4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.facebook.AccessToken;
+
 public class MainActivity extends AppCompatActivity {
+
+    AccessToken accessToken;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //check login
+        accessToken = AccessToken.getCurrentAccessToken();
+        if(accessToken == null || accessToken.isExpired()) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
