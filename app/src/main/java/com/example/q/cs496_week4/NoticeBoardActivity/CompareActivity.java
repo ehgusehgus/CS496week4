75d9ea80dc4bccde6290ba8e6566e19063bd9508
KDH <ehgus1657@kaist.ac.kr>
2018-07-25 20:51:00 +0900

    vote!!!!

--- app/src/main/java/com/example/q/cs496_week4/NoticeBoardActivity/CompareActivity.java
+++ app/src/main/java/com/example/q/cs496_week4/NoticeBoardActivity/CompareActivity.java
@@ -1,20 +1,18 @@
 package com.example.q.cs496_week4.NoticeBoardActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.q.cs496_week4.DetailSearchActivity.EmptySearchActivity;
-import com.example.q.cs496_week4.DetailSearchActivity.SearchActivity;
 import com.example.q.cs496_week4.HttpInterface;
 import com.example.q.cs496_week4.R;
-import com.google.gson.JsonArray;
+import com.facebook.AccessToken;
 import com.google.gson.JsonObject;
 
 import java.net.URLEncoder;
-import java.util.ArrayList;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -24,18 +22,21 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CompareActivity extends AppCompatActivity {
 
+    AccessToken accessToken;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compare);
 
+        accessToken = AccessToken.getCurrentAccessToken();
 
         Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create())
                 .baseUrl(HttpInterface.BaseURL)
                 .build();
-        HttpInterface httpInterface = retrofit.create(HttpInterface.class);
+        final HttpInterface httpInterface = retrofit.create(HttpInterface.class);
 
-        String keyword= getIntent().getExtras().getString("keyword");
+        final String keyword= getIntent().getExtras().getString("keyword");
         Log.d("TTTTTT", keyword);
 
         Call<JsonObject> getNoticeDetailCall = httpInterface.getNoticeDetail(URLEncoder.encode(keyword));
@@ -44,7 +45,14 @@ public class CompareActivity extends AppCompatActivity {
             public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
                 Log.d("??????", response.body().toString());
                 try {
+                    try{
+
+                    }catch (Exception e){
+
+                    }
+
                 }catch(Exception e){
+
                 }
             }
 
@@ -54,7 +62,70 @@ public class CompareActivity extends AppCompatActivity {
             }
         });
 
+        Call<JsonObject> getVoted = httpInterface.getVoted(URLEncoder.encode(keyword), accessToken.getUserId());
+        getVoted.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
 
+                try {
+                    Integer check = response.body().get("result").getAsInt();
+                    if(check==1)
+                        findViewById(R.id.vote).setVisibility(View.GONE);
+                    else
+                        findViewById(R.id.vote).setVisibility(View.VISIBLE);
+                }catch(Exception e){
+                    Log.d("&&&", "&&&&");
+                }
+            }
 
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+                Toast.makeText(getApplication(), "FAILURE", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button agree_but = (Button) findViewById(R.id.agree);
+        agree_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<JsonObject> Vote = httpInterface.Vote(keyword, accessToken.getUserId(), "1");
+                Vote.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        try {
+                            findViewById(R.id.vote).setVisibility(View.GONE);
+                        } catch (Exception e) {
+
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        Toast.makeText(getApplication(), "FAILURE", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
+        Button disagree_but = (Button) findViewById(R.id.disagree);
+        agree_but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Call<JsonObject> Vote = httpInterface.Vote(keyword, accessToken.getUserId(), "0");
+                Vote.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        try {
+                            findViewById(R.id.vote).setVisibility(View.GONE);
+                        } catch (Exception e) {
+
+                        }
+                    }
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        Toast.makeText(getApplication(), "FAILURE", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
     }
 }
